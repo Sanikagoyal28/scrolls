5f8522c08ed21e9bf8f2c0f076096f6c69532d11
sanika <sanika2110118@akgec.ac.in>
2023-03-27 19:30:55 +0530

    changes in registeration

--- src/Components/Register/team.jsx
+++ src/Components/Register/team.jsx
@@ -215,15 +215,15 @@ function Team() {
                 <p id="WrongPwdTeam2">Password entered in two fields must be same.</p>
                 <div className="teamLeader">
                     <p className="regName">Team Leader ID</p>
-                    <input type="text" className="regInputname" placeholder="Enter ID" value={team.leaderId} onChange={(e) => { setTeam({ ...team, leaderId: e.target.value }) }} />
+                    <input type="text" className="regInputname" placeholder="Enter ID" required={(team.size == 1 || team.size == 2 || team.size == 3)? true: false} value={team.leaderId} onChange={(e) => { setTeam({ ...team, leaderId: e.target.value }) }} />
                 </div>
                 <div className="teamLeader">
                     <p className="regName">Member 2 ID</p>
-                    <input type="text" className="regInputname" placeholder="Enter ID" value={team.member2} onChange={(e) => { setTeam({ ...team, member2: e.target.value }) }} />
+                    <input type="text" className="regInputname" placeholder="Enter ID" required={(team.size == 2 || team.size == 3) ? true: false} value={team.member2} onChange={(e) => { setTeam({ ...team, member2: e.target.value }) }} />
                 </div>
                 <div className="teamLeader">
                     <p className="regName">Member 3 ID</p>
-                    <input type="text" className="regInputname" placeholder="Enter ID" value={team.member3} onChange={(e) => { setTeam({ ...team, member3: e.target.value }) }} />
+                    <input type="text" className="regInputname" placeholder="Enter ID" required={(team.size == 3) ? true: false} value={team.member3} onChange={(e) => { setTeam({ ...team, member3: e.target.value }) }} />
                 </div>
                 <button className="regButton" onClick={RegAsTeam}>Register</button>
             </form>
